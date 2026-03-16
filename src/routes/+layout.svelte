8e1d32645daa8536e9b2f0e2134a831d41f79bf5
kisram <s2kisram@uni-trier.de>
2026-03-16 15:06:54 +0100

    change adapter to static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
-  import Header from '$lib/components/Header.svelte';
+  import Header from "$lib/components/Header.svelte";
   import "./layout.css";
-  import favicon from '$lib/assets/favicon.svg';
-import Footer from "$lib/components/Footer.svelte";
+  import favicon from "$lib/assets/favicon.svg";
+  import Footer from "$lib/components/Footer.svelte";
   let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} />
-</svelte:head>
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <div class="flex flex-col min-h-screen">
-  <Header/>
+  <Header />
   <main class="flex-1">
     <div class="container mx-auto px-6 md:px-8">
       {@render children()}
     </div>
   </main>
-  <Footer/>
+  <Footer />
 </div>
