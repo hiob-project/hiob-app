d9f6fa5caf4fdbf481f17a22ce83a8ce35bf5030
kisram <s2kisram@uni-trier.de>
2026-03-16 15:44:56 +0100

    fix: handle prerendering of dynamic routes

--- src/routes/midrash/[id]/+page.server.ts
+++ src/routes/midrash/[id]/+page.server.ts
@@ -2,6 +2,11 @@ import midrash from "$lib/data/midrash.json";
 import type { PageServerLoad } from "./$types";
 import { error } from "@sveltejs/kit";
 
+export const entries = () => {
+  // Need this to have a list of paths to prerender for dynamic routes.
+  return Object.values(midrash).map(item => ({ id: item.hiob_id }));
+};
+
 export const load: PageServerLoad = ({ params }) => {
   const midrashItem = Object.values(midrash).find(m => m.hiob_id === params.id);
   
--- src/routes/verses/[id]/+page.server.ts
+++ src/routes/verses/[id]/+page.server.ts
@@ -2,6 +2,11 @@ import verses from "$lib/data/verses.json";
 import type { PageServerLoad } from "./$types";
 import { error } from "@sveltejs/kit";
 
+export const entries = () => {
+  // Need this to have a list of paths to prerender for dynamic routes.
+  return Object.values(verses).map(verse => ({ id: verse.hiob_id }));
+};
+
 export const load: PageServerLoad = ({ params }) => {
   const verse = Object.values(verses).find(v => v.hiob_id === params.id);
   
