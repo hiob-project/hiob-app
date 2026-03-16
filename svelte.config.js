d81d56be23ee591479255e0bdbd5f89f9a03139b
kisram <s2kisram@uni-trier.de>
2026-03-16 16:33:49 +0100

    try to fix error with base path for "404"

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,28 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
-const dev = process.env.NODE_ENV === 'development';
-const base = dev ? '' : process.env.BASE_PATH || '';
+const dev = process.env.NODE_ENV === "development";
+const base = dev ? "" : process.env.BASE_PATH || "";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: base
-		},
-		prerender: {
-			entries: ['*']
-		}
-	},
-	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
-	}
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: base,
+    },
+    prerender: {
+      entries: base ? [base, `${base}/`] : ["*"],
+    },
+  },
+  vitePlugin: {
+    dynamicCompileOptions: ({ filename }) => (filename.includes("node_modules") ? undefined : { runes: true }),
+  },
 };
 
 export default config;
