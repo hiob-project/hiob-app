513023aca0a72780ecb647f60b7bc4752180e71a
kisram <s2kisram@uni-trier.de>
2026-04-17 14:48:14 +0200

    fix some colour scheme related issues

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
   import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
-  import { navigationMenuTriggerStyle } from "$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte";
-  import { Navigation } from "@lucide/svelte";
   import { resolve } from "$app/paths";
 </script>
 
 <header class="sticky top-0 z-50 w-full bg-background">
-  <div class="container-wrapper px-6 3xl:fixed:px-0 bg-secondary">
-    <NavigationMenu.Root>
-      <NavigationMenu.List>
+  <div class="container-wrapper px-6 3xl:fixed:px-0 bg-primary text-primary-foreground">
+    <NavigationMenu.Root viewport={false}>
+      <NavigationMenu.List class="flex-wrap">
         <NavigationMenu.Item>
-          <NavigationMenu.Link href={resolve("/")}>
+          <NavigationMenu.Link href={resolve("/")} class="hover:bg-transparent hover:text-accent hover:ring-1 hover:ring-accent focus:bg-transparent focus:text-accent focus:ring-1 focus:ring-accent">
             <span class="font-semibold text-lg tracking-wide">HIOB</span>
           </NavigationMenu.Link>
         </NavigationMenu.Item>
