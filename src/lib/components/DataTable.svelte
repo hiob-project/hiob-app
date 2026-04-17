0407b51e7f5010c169bbb7f9e7ca0d99d7ea4bf1
kisram <s2kisram@uni-trier.de>
2026-04-17 14:10:57 +0200

    feat_ add more detailed table footer

--- src/lib/components/DataTable.svelte
+++ src/lib/components/DataTable.svelte
@@ -3,8 +3,13 @@
   import { type ColumnDef, type PaginationState, type SortingState, getCoreRowModel, getPaginationRowModel, getSortedRowModel } from "@tanstack/table-core";
   import { createSvelteTable, FlexRender } from "$lib/components/ui/data-table/index.js";
   import * as Table from "$lib/components/ui/table/index.js";
-  import { Button } from "$lib/components/ui/button/index.js";
+  import * as Select from "$lib/components/ui/select/index.js";
 
+  import { Button } from "$lib/components/ui/button/index.js";
+  import ChevronRightIcon from "@lucide/svelte/icons/chevron-right";
+  import ChevronLeftIcon from "@lucide/svelte/icons/chevron-left";
+  import ChevronsLeftIcon from "@lucide/svelte/icons/chevrons-left";
+  import ChevronsRightIcon from "@lucide/svelte/icons/chevrons-right";
   type DataTableProps = {
     data: TData[];
     columns: ColumnDef<TData, TValue>[];
@@ -83,8 +88,55 @@
       </Table.Body>
     </Table.Root>
   </div>
-  <div class="flex items-center justify-end space-x-2 py-4">
-    <Button variant="outline" size="sm" onclick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>Previous</Button>
-    <Button variant="outline" size="sm" onclick={() => table.nextPage()} disabled={!table.getCanNextPage()}>Next</Button>
+
+  <div class="flex items-center justify-between px-2 py-4">
+    <div class="text-muted-foreground flex-1 text-sm">
+      Showing {table.getFilteredRowModel().rows.length} of
+      {table.getCoreRowModel().rows.length} row(s)
+    </div>
+    <div class="flex items-center space-x-6 lg:space-x-8">
+      <p class="text-sm font-medium">Rows per page</p>
+      <Select.Root
+        allowDeselect={false}
+        type="single"
+        value={`${table.getState().pagination.pageSize}`}
+        onValueChange={(value) => {
+          table.setPageSize(Number(value));
+        }}
+      >
+        <Select.Trigger class="h-8 w-[70px]">
+          {String(table.getState().pagination.pageSize)}
+        </Select.Trigger>
+        <Select.Content side="top">
+          {#each [10, 20, 30, 40, 50] as pageSize (pageSize)}
+            <Select.Item value={`${pageSize}`}>
+              {pageSize}
+            </Select.Item>
+          {/each}
+        </Select.Content>
+      </Select.Root>
+    </div>
+    <div class="flex w-[100px] items-center justify-center text-sm font-medium">
+      Page {table.getState().pagination.pageIndex + 1} of
+      {table.getPageCount()}
+    </div>
+    <div class="flex items-center space-x-2">
+      <Button variant="outline" class="hidden size-8 p-0 lg:flex" onclick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
+        <span class="sr-only">Go to first page</span>
+        <ChevronsLeftIcon />
+      </Button>
+      <Button variant="outline" class="size-8 p-0" onclick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+        <span class="sr-only">Go to previous page</span>
+        <ChevronLeftIcon />
+      </Button>
+      <Button variant="outline" class="size-8 p-0" onclick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+        <span class="sr-only">Go to next page</span>
+        <ChevronRightIcon />
+      </Button>
+      <Button variant="outline" class="hidden size-8 p-0 lg:flex" onclick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
+        <span class="sr-only">Go to last page</span>
+        <ChevronsRightIcon />
+      </Button>
+    </div>
   </div>
 </div>
